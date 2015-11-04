685366b5e21af66d6d16362ecf4d69807df58d65
Chris H-C <chutten@mozilla.com>
2015-11-04 14:22:33 -0500

    use the new telemetry-wrapper to render inline. Much faster

--- generator.js
+++ generator.js
@@ -43,75 +43,10 @@ const E10S_HISTOGRAMS = [
   'CYCLE_COLLECTOR_TIME_BETWEEN',
 ];
 
-const BASE_URL = 'http://chutten.github.io/telemetry-dashboard/new-pipeline/';
-// TODO: when &bare=true hits gh-pages, use the following instead:
-// const BASE_URL = 'http://telemetry.mozilla.org/new-pipeline/';
-const DIST_OPTIONS = {
-  compare: 'e10sEnabled',
-  cumulative: 0,
-  max_channel_version: 'nightly%252F44', // TODO: parameterize this from query
-  measure: '',
-  min_channel_version: 'null',
-  product: 'Firefox',
-  sanitize: 1,
-  trim: 1,
-  bare: true,
-};
-
-const EVO_OPTIONS = {
-  max_channel_version: 'nightly%252F44', // TODO: parameterize
-  measure: '',
-  min_channel_version: 'nightly%252F44', // ditto
-  product: 'Firefox',
-  sanitize: '1',
-  trim: '1',
-  bare: true,
-};
-
-var applyOptionsToUrl = function (url, options, measure) {
-  for (name in options) {
-    var value = options[name];
-    if (name == 'measure') {
-      value = measure;
-    }
-    url += name + '=' + value + '&';
-  }
-  return url.slice(0, -1);
-};
-
 window.addEventListener('load', function () {
 
-  var explanationEl = document.createElement('p');
-  explanationEl.className = 'explanation';
-  explanationEl.textContent = 'Loading ' + E10S_HISTOGRAMS.length + ' (times 2) histograms. Please be patient.';
-  document.body.appendChild(explanationEl);
-
-  var measuresContainerEl = document.createElement('div');
-  measuresContainerEl.className = 'measures-container';
-  document.body.appendChild(measuresContainerEl);
-
   for (histogram of E10S_HISTOGRAMS) {
-    var measureEl = document.createElement('div');
-    var headerEl = document.createElement('h2');
-    var distEl = document.createElement('iframe');
-    var evoEl = document.createElement('iframe');
-
-    headerEl.textContent = histogram;
-    headerEl.className = 'measure-header';
-    measureEl.appendChild(headerEl);
-
-    var distUrl = BASE_URL + 'dist.html#!';
-    distEl.src = applyOptionsToUrl(distUrl, DIST_OPTIONS, histogram);
-    distEl.className = 'dist-frame';
-    measureEl.appendChild(distEl);
-
-    var evoUrl = BASE_URL + 'evo.html#!';
-    evoEl.src = applyOptionsToUrl(evoUrl, EVO_OPTIONS, histogram);
-    evoEl.className = 'evo-frame';
-    measureEl.appendChild(evoEl);
-
-    measureEl.className = 'measure';
-    measureEl.measure = histogram; // in case we need it later
-    measuresContainerEl.appendChild(measureEl);
+    var twParams = $.extend({metric: histogram, compare: 'e10sEnabled'}, params);
+    TelemetryWrapper.go(twParams, document.body);
   }
 });
